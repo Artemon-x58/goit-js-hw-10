6becb282065cd3aa3776f489e0bafc7e97299e10
Artemon-x58 <artem1145819@gmail.com>
2023-07-01 22:34:09 +0200

    1.1

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,12 @@
+import axios from "axios";
+import Notiflix from 'notiflix';
+
 export function fetchBreeds () {
     return axios.get('https://api.thecatapi.com/v1/breeds').then(response => {
-      errorEl.classList.add("is-hidden");
-      
-    return  response.data
-    
+    //   errorEl.classList.add("is-hidden");
+    return  response.data    
   }).catch(err => {
-    loader.classList.add("is-hidden");
+    // loader.classList.add("is-hidden");
     Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!");
     })
   };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
-import { fetchCatByBreed } from "./cat-api";
+import {fetchBreeds} from "./cat-api";
 
 axios.defaults.headers.common["x-api-key"] = "live_zLujiNOCDiWSjdGB1YUgqrKxYa2muYh5iurPJARB5ldgMIYXuZnxa876lyRjWeLx";
-
-
 
 const selectEl = document.querySelector('.breed-select');
 const contEl = document.querySelector('.cat-info');
@@ -24,7 +22,6 @@ content.classList.add("content")
 content.append(textChoose)
 document.body.insertAdjacentElement("afterbegin", content)
 
-
 selectEl.classList.add("is-hidden");
 imgKat.width = '600';
 imgKat.height = '600';
@@ -33,23 +30,19 @@ imgKat.style.borderRadius = "10px"
 
 fetchBreeds()
 .then(breeds => {
+  errorEl.classList.add("is-hidden");
   
   textChoose.classList.add("choose");
 textChoose.textContent = "Please choose a cat"
-// document.body.appendChild(textChoose);
 
-  
   breeds.forEach(breed => {
     const option = document.createElement("option");
     option.value = breed.id ;
     option.textContent = breed.name;
-    
     selectEl.appendChild(option);
   });
   selectEl.classList.remove("is-hidden");
   loader.classList.toggle("is-hidden");
-  
-  
 });
 
 selectEl.addEventListener('change', (e) => {
@@ -58,7 +51,6 @@ selectEl.addEventListener('change', (e) => {
   textChoose.classList.add("is-hidden");
     
     const breedId = e.srcElement.value;
-    // console.log(breedId)
     axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`).then(response => {
       errorEl.classList.add("is-hidden");
         
@@ -74,8 +66,6 @@ selectEl.addEventListener('change', (e) => {
         contEl.append(imgKat, wrapEl);
         wrapEl.style.marginLeft = "15px"
         
-
-        // console.log(response.data[0]);
         loader.classList.toggle("is-hidden");
         contEl.style.display = "flex";
         contEl.style.marginTop = "15px";
@@ -85,8 +75,6 @@ selectEl.addEventListener('change', (e) => {
       Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!")
     });
 });
-
-
 
 function makeMusic () {
   const audio = document.querySelector("#music");
